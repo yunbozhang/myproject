5f09befccebbc79a4594b035858d3bdc9b85c51c
ybzhang <286267616@qq.com>
2016-10-17 09:49:30 +0800

    初始创建

---
+++ system/caches/caches_log/admin.log.2016-10-17.php
@@ -0,0 +1,11 @@
+<?php exit; ?> 
+
+
+
+2016-10-17 09:34:07------>array (
+  0 => 'UPDATE `go_admin` SET `logintime`=\'1476668047\' WHERE (`uid`=\'1\')',
+  1 => 'UPDATE `go_admin` SET `loginip`=\'127.0.0.1\' WHERE (`uid`=\'1\')',
+) 
+
+
+
---
+++ system/config/database.inc.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'default' => array (
+'hostname' => 'localhost','database' => 'chenghedao','username' => 'root','password' => '','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+);
