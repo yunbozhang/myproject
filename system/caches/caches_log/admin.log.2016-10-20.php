39f1d3a5cd415b20e9456e7e3e727241981c0828
ybzhang <286267616@qq.com>
2016-10-21 08:59:59 +0800

    修改 底部切换  首页  登录  注册

--- system/caches/caches_log/admin.log.2016-10-20.php
+++ system/caches/caches_log/admin.log.2016-10-20.php
@@ -16,3 +16,10 @@
 
 
 
+2016-10-20 18:48:39------>array (
+  0 => 'UPDATE `go_admin` SET `logintime`=\'1476960518\' WHERE (`uid`=\'1\')',
+  1 => 'UPDATE `go_admin` SET `loginip`=\'127.0.0.1\' WHERE (`uid`=\'1\')',
+) 
+
+
+
--- system/config/domain.inc.php
+++ system/config/domain.inc.php
@@ -1,13 +1,7 @@
 <?php 
 
 return array (
-  'www.yunbozhang.com' => 
-  array (
-    'm' => 'mobile',
-    'c' => 'mobile',
-    'a' => 'init',
-  ),
-  'www.myself.com' => 
+  'www.my.com' => 
   array (
     'm' => 'mobile',
     'c' => 'mobile',
