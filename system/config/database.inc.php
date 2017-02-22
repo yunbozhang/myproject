1ec7755783a0711103cddc69154d6b19ce765655
icq <286267616@qq.com>
2017-02-22 15:41:31 +0800

    修改数据库

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'default' => array (
-'hostname' => 'localhost','database' => 'chenghedao','username' => 'root','password' => '','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+'hostname' => 'localhost','database' => 'chenghedao','username' => 'root','password' => 'a92e541e15','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
 );
